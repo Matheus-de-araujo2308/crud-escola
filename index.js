a0a20ce630509cc8464dff76dcc00aac80a219b3
NatanaelTRodrigues <natastavaresrodrigues@gmail.com>
2025-10-01 16:46:09 -0300

    Inicio projeto

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-// TODO: Membro 1 - Importar e mapear rota de alunos
 
-// TODO: Membro 2 - Importar e mapear rota de professores
+const alunos = require('./routes/Alunos')
+const professores = require ('./routes/Professores')
+
+app.use('./alunos', alunos);
+app.use('./professores', professores)
 
 app.listen(3000, () => {
 console.log('Server is running on http://localhost:3000')
