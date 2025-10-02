f85985d8ab8d6bf857934e42cc2e01de88e5c49a
NatanaelTRodrigues <natastavaresrodrigues@gmail.com>
2025-10-01 22:04:01 -0300

    README

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const alunos = require('./routes/Alunos')
+const alunos = require('./routes/alunos')
 const professores = require ('./routes/Professores')
 
-app.use('./alunos', alunos);
-app.use('./professores', professores)
+app.use('/alunos', alunos);
+app.use('/professores', professores)
 
 app.listen(3000, () => {
 console.log('Server is running on http://localhost:3000')
